9ca91cc271dc7e1496d305c43cdd334e43f7c42e
Bigs <matthieu.guenebeaud@kirin-consulting.fr>
2022-03-10 22:52:51 +0100

    add quasar front

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./plugins/router";
 import store from "./plugins/store";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(Quasar, quasarUserOptions)
+  .use(store)
+  .use(router)
+  .mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = {
+  pluginOptions: {
+    quasar: {
+      importStrategy: "kebab",
+      rtlSupport: false,
+    },
+  },
+  transpileDependencies: ["quasar"],
+};
